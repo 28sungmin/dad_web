40cd46c6b8e35483364aa1d7b252fba1f799da16
28sungmin <gooddays4001@naver.com>
2024-06-25 14:13:46 +0900

    build

--- src/pages/IntroPg.jsx
+++ src/pages/IntroPg.jsx
@@ -1,8 +1,42 @@
-const introPg = () => {
+import { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import ImgAtm from "../components/atom/ImgAtm";
+
+import slide1 from "../assets/images/slide1.png";
+import slide2 from "../assets/images/slide2.png";
+import slide3 from "../assets/images/slide3.png";
+
+import { click } from "@testing-library/user-event/dist/click";
+
+const MainPg = () => {
+  const slideLists = [slide1, slide2, slide3];
+  // const [slideLists, setslideLists] = useState([slide1, slide2, slide3]);
+
+  const settings = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+  };
+
   return (
-    <>
-      <h3>intro Page</h3>
-    </>
+    <section id="main">
+      <Slider {...settings}>
+        <div>
+          <ImgAtm srcProp={slide1} />
+        </div>
+        <div>
+          <ImgAtm srcProp={slide2} />
+        </div>
+        <div>
+          <ImgAtm srcProp={slide3} />
+        </div>
+      </Slider>
+    </section>
   );
 };
-export default introPg;
+export default MainPg;
--- src/pages/MainPg.jsx
+++ src/pages/MainPg.jsx
@@ -1,4 +1,8 @@
 import { useState, useEffect } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import ImgAtm from "../components/atom/ImgAtm";
 import TitleAtm from "../components/atom/TitleAtm";
@@ -6,28 +10,88 @@ import TextAtm from "../components/atom/TextAtm";
 
 import mainImg from "../assets/images/main.png";
 import mainImg2 from "../assets/images/main2.png";
+
 import slide1 from "../assets/images/slide1.png";
 import slide2 from "../assets/images/slide2.png";
 import slide3 from "../assets/images/slide3.png";
-// import slide4 from "../assets/images/slide4.png";
+import slide4 from "../assets/images/slide4.png";
+import slide5 from "../assets/images/slide5.png";
+
 import intro1 from "../assets/images/intro1.png";
 import intro2 from "../assets/images/intro2.png";
 import intro3 from "../assets/images/intro3.png";
 import intro4 from "../assets/images/intro4.png";
 
 const MainPg = () => {
-  const slideLists = [slide1, slide2, slide3];
+  const slideLists = [slide1, slide2, slide3, slide4, slide5];
+  // const [slideLists, setslideLists] = useState([slide1, slide2, slide3]);
   const introLists = [intro1, intro2, intro3, intro4];
-
-  const angle = 360 / introLists.length;
+  // const [intros, setIntros] = useState([intro1, intro2, intro3, intro4]);
 
   // setActivateImg()는 activateImg 변수의 상태를 업데이트하는 함수
-  const [activateImg, setActivateImg] = useState(null);
-  const [currentImg, setCurrentImg] = useState(0);
+  // const [activateImg, setActivateImg] = useState(null);
 
-  const imgCheck = (index) => {
-    setActivateImg(index);
+  // const imgCheck = (index) => {
+  //   setActivateImg(index);
+  // };
+
+  const settings = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
   };
+
+  const StyledSlider = styled(Slider)`
+    .slick-prev {
+      left: -40px;
+      &:before {
+        color: rgb(0, 132, 255);
+        font-size: 32px;
+      }
+    }
+    .slick-next {
+      right: -30px;
+      &:before {
+        color: rgb(0, 132, 255);
+        font-size: 32px;
+      }
+    }
+  `;
+
+  // const handleNextClick = () => {
+  //   setslideLists((sLists) => {
+  //     const [first, ...rest] = sLists;
+  //     return [...rest, first];
+  //   });
+  // };
+
+  // const handlePrevClick = () => {
+  //   setslideLists((sLists) => {
+  //     const lastElement = sLists[sLists.length - 1];
+  //     const newList = sLists.slice(0, sLists.length - 1);
+  //     return [lastElement, ...newList];
+  //   });
+  // };
+
+  // const handleImgClick = (index) => {
+  //   setslideLists((sLists) => {
+  //     const lastElement = sLists[sLists.length - 1];
+  //     const newList = sLists.slice(0, sLists.length - 1);
+  //     setDirection("prev");
+  //     console.log(index);
+  //     return [lastElement, ...newList];
+  //   });
+  // };
+
+  // const handleNextClick = () => {
+  //   const firstElement = slideLists[0];
+  //   const newList = slideLists.slice(1);
+  //   newList.push(firstElement);
+  //   console.log(newList);
+  //   setslideLists(newList);
+  // };
 
   return (
     <section id="main">
@@ -47,24 +111,20 @@ const MainPg = () => {
         <ImgAtm srcProp={mainImg} altProp="메인 가족 사진" />
         <ImgAtm srcProp={mainImg2} altProp="메인 가족 사진" />
       </div>
-      <div className="slider_all mx-auto position-relative">
-        <div className="slider_img mx-auto d-flex justify-content-center position-absolute">
+      {/* <div className="nextBtn d-flex justify-content-center">
+        <button onClick={handlePrevClick}>이전</button>
+      </div>
+      <div className="nextBtn d-flex justify-content-center">
+        <button onClick={handleNextClick}>다음</button>
+      </div> */}
+      <div className="slider_all mx-auto ">
+        <StyledSlider {...settings}>
           {slideLists.map((v, i) => (
-            <div
-              className={`slider ${
-                activateImg === i ? "effect" : ""
-              } position-absolute `}
-              key={i}
-              onClick={() => imgCheck(i)}
-            >
-              <ImgAtm
-                className={`img${i}`}
-                srcProp={v}
-                altProp={`${v} 이미지`}
-              />
+            <div className="slider" key={i}>
+              <ImgAtm srcProp={v} />
             </div>
           ))}
-        </div>
+        </StyledSlider>
       </div>
     </section>
   );
